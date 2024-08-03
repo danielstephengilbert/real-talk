cecfb77af8607ec458dd16f7ee40cb553bd25e02
Daniel Gilbert <121529996+wwwdanielg@users.noreply.github.com>
2024-08-02 17:57:10 -0700

    Write socket code

---
+++ include/socket.h
@@ -0,0 +1,7 @@
+#ifndef SOCKET_H_
+#define SOCKET_H_
+
+int create_socket();
+void destroy_socket(int sockfd);
+
+#endif
---
+++ src/socket.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int create_socket() {
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (sockfd == -1) {
+    fprintf(stderr, "\nFailed to create socket.\n");
+    exit(1);
+  }
+
+  return sockfd;
+
+}
+
+void destroy_socket(int sockfd) {
+
+  int status = close(sockfd);
+
+  if (status != 0) {
+    fprintf(stderr, "\nFailed to close socket.\n");
+    exit(1);
+  }
+
+}
