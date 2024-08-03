13634e5bba45217a35ef4cf7a22e8f367d1cc387
Daniel Gilbert <121529996+wwwdanielg@users.noreply.github.com>
2024-08-02 18:20:48 -0700

    Modify naming convention for arguments

--- src/rt.c
+++ src/rt.c
@@ -1,16 +1,18 @@
+#include <sys/socket.h>
+
 #include "../include/validation.h"
 
 int main(int argc, char *argv[]) {
 
   validate_arg_count(argc);
 
-  char *mode = argv[1];
-  char *ip   = argv[2];
-  char *port = argv[3];
+  char *_mode = argv[1];
+  char *_ip   = argv[2];
+  char *_port = argv[3];
 
-  validate_mode(mode);
-  validate_ip(ip);
-  validate_port(port);
+  validate_mode(_mode);
+  validate_ip(_ip);
+  validate_port(_port);
 
   return 0;
 
