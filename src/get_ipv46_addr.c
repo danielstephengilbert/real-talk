9cc15694905218ee21d943377d089470374e07bc
Daniel Gilbert <121529996+danielstephengilbert@users.noreply.github.com>
2024-08-11 16:52:18 -0700

    Remove whitespace

--- src/get_ipv46_addr.c
+++ src/get_ipv46_addr.c
@@ -31,26 +31,20 @@ const void* get_ipv46_addr(
 
   // If IPv4 address, get the correct struct format.
   if (is_ipv4) {
-
     ipv4        = (ipv4_struct*) addr;
     ipv4_addr   = &(ipv4->sin_addr);
-
     return ipv4_addr;
 
   // If IPv6 address, get the correct struct format.
   } else if (is_ipv6) {
-
     ipv6        = (ipv6_struct*) addr;
     ipv6_addr   = &(ipv6->sin6_addr);
-
     return ipv6_addr;
 
   // If neither IPv4 or IPv6, throw error.
   } else {
-
     fprintf(stderr, "Error: Could not obtain address.\n");
     exit(1);
-
   }
 
 }
