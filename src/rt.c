4f033c7e5b0db15a782cc91c16e49d90ba66be2e
Daniel Gilbert <121529996+danielstephengilbert@users.noreply.github.com>
2024-08-10 23:23:58 -0700

    Reorder main var declarations and add comments

--- src/rt.c
+++ src/rt.c
@@ -47,63 +47,80 @@ void process_client(
 
 int main(void) {
 
-  int     max_conns;
+  // Connection data structures.
   conn*   conns;
+  conn*   conn;
+
+  // Connection-related data.
+  int     conn_index;
+  int     conn_count;
+  int     max_conns;
   int     conns_size;
+
+  // Listener variables.
   int     listener_fd;
   int     listener_error;
-  int     conn_count;
+
+  // Poll variables.
   int     poll_val;
   int     poll_error;
-  int     conn_index;
-  conn*   conn;
+
+  // Data ready flag info.
   int     data_ready;
   int     listener_data;
   int     client_data;
 
+  // Allocate memory for connections.
   max_conns   = 5;
   conns_size  = sizeof(conn) * max_conns;
   conns       = malloc(conns_size);
 
+  // Get a listener to receive connections on the server.
   listener_fd     = get_listener();
   listener_error  = (listener_fd == -1);
 
+  // Throw error if cannot get listener.
   if (listener_error) {
     fprintf(stderr, "Error: Could not start listener.\n");
     exit(1);
   }
 
+  // Make the listener the first connection on the server.
   conns[0].fd       = listener_fd;
   conns[0].events   = POLLIN;
-
   conn_count        = 1;
 
+  // Infinite loop to keep server running until terminated.
   while (1) {
 
+    // Retrieve connection information for all connections
+    // to see if any of them have data ready.
     poll_val    = poll(conns, conn_count, -1);
     poll_error  = (poll_val == -1);
 
+    // Throw error if could not retrive connection information.
     if (poll_error) {
       perror("poll");
       exit(1);
     }
 
+    // Check every connection to see if it has data ready.
     for (conn_index = 0; conn_index < conn_count; conn_index++) {
 
+      // Get the current connection.
       conn          = &conns[conn_index];
       
+      // Check if data ready, and from which type of connection.
       data_ready    = (conn->revents & POLLIN);
       listener_data = (conn->fd == listener_fd);
       client_data   = !listener_data;
 
+      // If data is ready from the listener connection, process it.
       if (data_ready && listener_data) {
-
         process_listener(listener_fd, &conns, &conn_count, &max_conns);
-
+      // If data is ready from a client connection, process it.
       } else if (data_ready && client_data) {
-
         process_client(conn_index, &conns, &conn_count, listener_fd);
-
       }
 
     }
