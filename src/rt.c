4895d84e4d9d793271fb8adae1b3b3a716809532
Daniel Gilbert <121529996+danielstephengilbert@users.noreply.github.com>
2024-08-11 00:08:14 -0700

    Rome wasn't built in a day, probably due to useless comments

--- src/rt.c
+++ src/rt.c
@@ -378,7 +378,7 @@ void process_listener(
     
     addr_family     = client_addr.ss_family;
     addr_struct     = get_in_addr((address *) &client_addr);
-    
+
     client_addr_str = inet_ntop(
       addr_family, addr_struct, addr_buffer, max_addr_len
     );
@@ -391,6 +391,9 @@ void process_listener(
 
 }
 
+/**
+ *  Process the client connection.
+ */
 void process_client(
   int           conn_index,
   connection**  conns,
@@ -398,22 +401,33 @@ void process_client(
   int           listener_fd
 ) {
 
+  // Sender vars.
   connection    sender_conn;
   int           sender_fd;
+
+  // Buffer vars.
   int           buf_len = 256;
   char          buffer[buf_len];
+
+  // Data information.
   int           data_len;
   int           recv_error;
   int           disconnected;
+
+  // Connection info.
   int           client_fd;
   int           not_listener;
   int           not_sender;
+
+  // Send status.
   int           send_val;
   int           send_error;
 
+  // Get the sender connection.
   sender_conn = (*conns)[conn_index];
   sender_fd   = sender_conn.fd;
 
+  // Receive the data.
   data_len = recv(sender_fd, buffer, buf_len, 0);
 
   if (data_len <= 0) {
@@ -421,6 +435,7 @@ void process_client(
     recv_error    = (data_len < 0);
     disconnected  = (data_len == 0);
 
+    // Process errors related to receiving data or if the connection terminated.
     if (recv_error)   perror("recv");
     if (disconnected) printf(
       "Connection terminated from client on socket %d.\n", sender_fd
@@ -432,6 +447,7 @@ void process_client(
 
   } else {
 
+    // Send the data received to all of the client connections.
     for (conn_index = 0; conn_index < *conn_count; conn_index++) {
       
       client_fd = (*conns)[conn_index].fd;
