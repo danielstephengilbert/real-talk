50a0b3955f8909752903d47d68994fbdb31c7b7c
Daniel Gilbert <121529996+danielstephengilbert@users.noreply.github.com>
2024-08-11 17:00:27 -0700

    Reformat code

--- src/process_client.c
+++ src/process_client.c
@@ -46,39 +46,47 @@ void process_client(
   // Receive the data.
   data_len = recv(sender_fd, buffer, buf_len, 0);
 
+  // If data length indicates an error, handle the error then return;
   if (data_len <= 0) {
 
+    // Check specific error conditions.
     recv_error    = (data_len < 0);
     disconnected  = (data_len == 0);
 
-    // Process errors related to receiving data or if the connection terminated.
-    if (recv_error)   perror("recv");
+    // Throw error if could not receive data.
+    if (recv_error) perror("recv");
+
+    // Throw error if connection terminated.
     if (disconnected) printf(
       "Connection terminated from client on socket %d.\n", sender_fd
     );
 
+    // Close and remove the connection if any errors, then return.
     close(sender_fd);
-    
     remove_connection(conn_index, conns, conn_count);
+    return;
 
-  } else {
+  }
 
-    // Send the data received to all of the client connections.
-    for (conn_index = 0; conn_index < *conn_count; conn_index++) {
-      
-      client_fd = (*conns)[conn_index].fd;
+  // Send the data received to all of the client connections.
+  for (conn_index = 0; conn_index < *conn_count; conn_index++) {
+    
+    // Get the client connection.
+    client_fd = (*conns)[conn_index].fd;
 
-      not_listener = (client_fd != listener_fd);
-      not_sender   = (client_fd != sender_fd);
+    // Check if current connection listener or sender.
+    not_listener = (client_fd != listener_fd);
+    not_sender   = (client_fd != sender_fd);
 
-      if (not_listener && not_sender) {
+    // Do not send data to listener or sender.
+    if (not_listener && not_sender) {
 
-        send_val    = send(client_fd, buffer, data_len, 0);
-        send_error  = (send_val == -1);
+      // Send the data, and capture any error.
+      send_val    = send(client_fd, buffer, data_len, 0);
+      send_error  = (send_val == -1);
 
-        if (send_error) perror("send");
-
-      }
+      // Print error if cannot send.
+      if (send_error) perror("send");
 
     }
 
